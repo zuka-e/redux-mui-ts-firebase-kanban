9ba2b8c26fd5b9c0e2da5b1409a7bb7cabd593c9
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-03-14 14:04:13 +0900

    Extend urls and methods

--- app/polls/views.py
+++ app/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("Question_no = %s" % question_id)
+
+def results(request, question_id):
+    response = "Result of %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question %s" % question_id)
