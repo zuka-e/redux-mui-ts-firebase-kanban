060a8f836ee2e270c572ee6f8d576a55cc655639
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-03-15 23:31:54 +0900

    Add namespace & shortcut for Http

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls' # 名前空間 templatesでの'polls:~'を利用する
 urlpatterns = [
     path('', views.index, name='index'),
+    # polls/id/ 名前付きルート(detail): templatesで使用
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,16 +1,26 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {'latest_question_list': latest_question_list,}
+    # return HttpResponse(template.render(context, request))
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("Question_no = %s" % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # 要求されたIDが存在しなければ404例外, 上記と同じ(ショートカット)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question': question})
 
 def results(request, question_id):
     response = "Result of %s"
