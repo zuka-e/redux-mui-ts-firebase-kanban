5292573f64ea43a56f9bcfbc4486680f69a7a2a1
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-03-16 01:06:58 +0900

    Add form & redirect

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -20,11 +21,22 @@ def detail(request, question_id):
     #     raise Http404("Question does not exist")
     # 要求されたIDが存在しなければ404例外, 上記と同じ(ショートカット)
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "Result of %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("Vote on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist): # 選択肢選ばなかった場合
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You did't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1 # choiceモデルのvotesカラム
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
