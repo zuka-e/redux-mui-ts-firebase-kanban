2a4eb4c43a7ae4811c6aeb25e6fea3a953c26249
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-03-20 13:55:31 +0900

    Add models for todos

--- app/todos/admin.py
+++ app/todos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, TodoList, Card
+
+admin.site.register(User)
+admin.site.register(TodoList)
+admin.site.register(Card)
+
--- app/todos/models.py
+++ app/todos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+  name = models.CharField(max_length=20)
+  email = models.EmailField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
+
+class TodoList(models.Model):
+  user = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE)
+  title = models.CharField(max_length=30)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.title
+
+class Card(models.Model):
+  todo_list = models.ForeignKey(TodoList, related_name='todo_lists', on_delete=models.CASCADE)
+  title = models.CharField(max_length=30)
+  body = models.TextField()
+  limit = models.DateField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.title
+
