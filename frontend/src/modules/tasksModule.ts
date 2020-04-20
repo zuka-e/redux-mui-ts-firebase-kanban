5930f71ddecec7f7d5125f49a9a3ee77680f08c1
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-04-20 13:55:33 +0900

    Add a tasksModule with createSlice()

---
+++ frontend/src/modules/tasksModule.ts
@@ -0,0 +1,60 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Task } from "../components/Types";
+
+type State = {
+  count: number;
+  tasks: Task[];
+};
+
+const initialState: State = {
+  count: 2,
+  tasks: [
+    {
+      id: 2,
+      title: "次のTodo",
+      done: false,
+    },
+    {
+      id: 1,
+      title: "最初のTodo",
+      done: true,
+    },
+  ],
+};
+
+// createSlice: Action(Creator), Reducer, State 一括定義可
+// Actions: Storeに送る唯一の情報源, 例: {type: "name/addTask", payload: XXX}
+// Reducers: 前のState, Actionを引数に 次のStateを返す'純粋'関数
+// State: intitialState: Stateの初期値設定
+// Store: 全Stateの集合, 'dispatch(action)'によってStateの変更を行う
+const tasksModule = createSlice({
+  name: 'tasks',
+  initialState: initialState,
+  reducers: {
+    addTask: (state: State, action: PayloadAction<string>) => {
+      state.count++
+      const newTask: Task = {
+        id: state.count,
+        title: action.payload,
+        done: false
+      }
+      state.tasks = [newTask, ...state.tasks]
+    },
+    // 別の記述法
+    doneTask(state: State, action: PayloadAction<Task>) {
+      // action.payload: 引数取得
+      const task = state.tasks.find(t => t.id === action.payload.id)
+      if (task) {
+        task.done = !task.done
+      }
+    },
+    deleteTask(state: State, action: PayloadAction<Task>) {
+      state.tasks = state.tasks.filter(t => t.id !== action.payload.id)
+    }
+  }
+})
+
+// 他コンポーネントからaddTask()で実行可に cf.tasksModule.actions.addTask()
+export const { addTask, doneTask, deleteTask } = tasksModule.actions
+
+export default tasksModule
--- frontend/src/rootReducer.ts
+++ frontend/src/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import tasksModule from './modules/tasksModule'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+  tasks: tasksModule.reducer
+})
 
 export type RootState = ReturnType<typeof rootReducer>
 
