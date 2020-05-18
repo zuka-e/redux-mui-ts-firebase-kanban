832ed09dc533350e86141f066769c1b6e214f716
zuka-e <52391918+zuka-e@users.noreply.github.com>
2020-05-18 20:11:30 +0900

    Tried applying a global MUI theme

--- frontend/src/layouts/ThemeProvider.tsx
+++ frontend/src/layouts/ThemeProvider.tsx
@@ -22,8 +22,27 @@ import {
   grey,
 } from '@material-ui/core/colors';
 
+const globalTheme = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        padding: '2px 10px 2px 5px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+});
+
 const DEFAULT = 'Default';
 const defaultTheme = createMuiTheme({
+  ...globalTheme,
   palette: {
     primary: indigo,
     secondary: blue,
